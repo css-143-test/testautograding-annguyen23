6f6dca979f0c0cdcc80190e4d84777746a1370a1
annguyen23 <54456351+annguyen23@users.noreply.github.com>
2021-10-08 00:33:56 -0700

    Update Hello.java

--- src/main/java/com/example/project/Hello.java
+++ src/main/java/com/example/project/Hello.java
@@ -2,7 +2,7 @@ package com.example.project;
 
 public class Hello {
 
-	public static void main(final String[] args) {
+	public static void main( String[] args) {
 		System.out.println("Hello world!");
 	}
 
